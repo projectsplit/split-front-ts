a15b0717527f4b6e7debeb9b9bba711d984fb9d4
thanoskat <thanoskat@gmx.com>
2023-07-06 18:11:03 +0300

    refactor auth endpoints

---
+++ src/Components/Sessions/Sessions.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState } from 'react'
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { Link, useNavigate } from 'react-router-dom'
+import { authApi } from '../../apis/authApi'
+import { EmailInitiateRequest } from '../../types'
+import { AxiosResponse } from 'axios'
+import ContinueWithGoogleButton from '../ContinueWithGoogleButton/ContinueWithGoogleButton'
+
+export default function Sessions() {
+
+  const [signInEmail, setSignInEmail] = useState<string>("")
+  const [signInError, setSigninError] = useState<string>("")
+  const [networkError, setNetworkError] = useState<string>("")
+  const [googleUrl, setGoogleUrl] = useState<string>("")
+
+  const navigate = useNavigate()
+
+  const emailInitiateMutation = useMutation<AxiosResponse<any, any>, any, EmailInitiateRequest>({
+    mutationKey: ['emailInitiate'],
+    mutationFn: authApi.emailSendLink,
+    onSuccess: () => navigate('/email-continue'),
+    onError: (error) => {
+      if (error.code === "ERR_NETWORK") {
+        setNetworkError(error.message + ": Check your internet connection")
+      }
+      if (error.response.status === 400) {
+        if (Array.isArray(error.response.data)) setSigninError(error.response.data.at(-1).errorMessage)
+        else setSigninError(error.message)
+      }
+      if (error.response.status === 401) {
+        console.log(401)
+        setSigninError("No account associated with this email address was found")
+      }
+    }
+  })
+
+  const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInEmail(e.target.value)
+  }
+
+  const submitSignIn = async (e: React.FormEvent<HTMLButtonElement>, email: string) => {
+    e.preventDefault()
+    setSigninError("")
+    setNetworkError("")
+    emailInitiateMutation.mutate({
+      email: signInEmail
+    })
+  }
+
+  return (
+    <div>
+      <h3>Enter you email</h3>
+      <input
+        inputMode='email'
+        value={signInEmail}
+        placeholder='john@rambo.com'
+        onChange={e => changeEmail(e)}
+      />
+      <div>{signInError}</div>
+      <button
+        disabled={emailInitiateMutation.isLoading}
+        onClick={(e) => submitSignIn(e, signInEmail)}>
+        {emailInitiateMutation.isLoading ? "Loading..." : "Submit"}
+      </button>
+      <h3>OR</h3>
+      <div>{networkError}</div>
+      <ContinueWithGoogleButton />
+    </div >
+  )
+}
--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, AxiosRequestConfig } from 'axios'
 import { authApi } from './authApi'
 import jwt_decode, { JwtPayload } from 'jwt-decode'
 import { getAccessToken, setAccessToken } from '../util/accessToken'
@@ -8,6 +8,9 @@ import { signOut } from '../util/signOut'
 const apiHttpClient = axios.create({
   baseURL: `${process.env.REACT_APP_APIURL}`
 })
+
+let isRefreshing = false;
+let requestQueue: Array<(accessToken: string) => void> = [];
 
 const isAccessTokenDecodableAndNotExpired = (accessToken: string | null | undefined): boolean => {
   if (accessToken) {
@@ -28,23 +31,61 @@ const isAccessTokenDecodableAndNotExpired = (accessToken: string | null | undefi
 }
 
 apiHttpClient.interceptors.request.use(async (request: any) => {
+  
   const accessToken = getAccessToken()
+
   if (isAccessTokenDecodableAndNotExpired(accessToken)) {
     request.headers.Authorization = `Bearer ${accessToken}`
     return request
   }
-  try {
-    const { accessToken } = await authApi.refreshAccessToken()
-    setAccessToken(accessToken)
-    request.headers.Authorization = `Bearer ${accessToken}`
-    return request
+
+  if (isRefreshing) {
+    return queueRequest(request)
   }
-  catch (e) {
-    signOut()
-  }
+
+  return refreshAccessToken(request)
 },
   (error: AxiosError) => Promise.reject(error)
 )
+
+
+// A function to queue the requests
+function queueRequest(request: AxiosRequestConfig) {
+  
+  return new Promise((resolve, reject) => {
+    requestQueue.push((accessToken: string) => {
+      request.headers = request.headers || {}
+      request.headers.Authorization = `Bearer ${accessToken}`
+      resolve(request)
+    })
+  })
+}
+
+// A function to refresh the access token
+async function refreshAccessToken(request: AxiosRequestConfig) {
+  
+  isRefreshing = true
+
+  try {
+    const { accessToken: newAccessToken } = await authApi.refreshAccessToken()
+    setAccessToken(newAccessToken)
+    isRefreshing = false
+    processQueue(newAccessToken)
+    request.headers = request.headers || {}
+    request.headers.Authorization = `Bearer ${newAccessToken}`
+    return request
+  } catch (e) {
+    signOut()
+  }
+}
+
+// A function to process the queued requests
+function processQueue(newAccessToken: string) {
+  while (requestQueue.length) {
+    const process = requestQueue.shift() as (accessToken: string) => void
+    process(newAccessToken)
+  }
+}
 
 const getGroupById = async (groupId: string) => {
   const response = await apiHttpClient.get<GetGroupResponse>(`/group/get`, { params: { id: groupId } });
